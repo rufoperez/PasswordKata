c7f61ffd81dd5dbd9540de7444308cde95419e46
Rufino Perez <ruperez@aidacanarias.com>
2022-06-09 09:33:10 +0200

    Add project files.

---
+++ PasswordKataTest/PasswordKataTest.cs
@@ -0,0 +1,16 @@
+namespace PasswordKataTest
+{
+    public class PasswordKataTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
