e4c73eff63973be16a09951f326b73335f4519f0
Rufino Perez <ruperez@aidacanarias.com>
2022-06-09 09:39:28 +0200

    green when_password_is_8_characters_lenght_return_true

--- PasswordKata/Password.cs
+++ PasswordKata/Password.cs
@@ -4,6 +4,8 @@ public class Password
 {
     public static bool Validation(string givenPassword)
     {
+        if (givenPassword.Length >= 8)
+            return true;
         return false;
     }
 }
