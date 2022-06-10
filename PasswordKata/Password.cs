f013aba994477e39ea2068287c457d265e92bce3
Rufino Perez <ruperez@aidacanarias.com>
2022-06-10 08:49:42 +0200

    green when_password_is_less_than_two_numbers_return_error

--- PasswordKata/Password.cs
+++ PasswordKata/Password.cs
@@ -4,8 +4,21 @@ public class Password
 {
     public static bool Validation(string givenPassword)
     {
-        if (givenPassword.Length >= 8)
-            return true;
-        throw new ArgumentException("Password must be at least 8 characters");
+        if (givenPassword.Length < 8)
+            throw new ArgumentException("Password must be at least 8 characters");
+
+        var numbersInPassword = 0;
+        foreach (char c in givenPassword)
+        {
+            var result = 0;
+            if(int.TryParse(c.ToString(), out result))
+                numbersInPassword++;
+        }
+
+        if (numbersInPassword <2)
+            throw new ArgumentException("Password must contain at least two numbers");
+
+        return true;
+
     }
 }
