99aaa19517a07c6a0b45469dd5088dc328f8afbd
Rufino Perez <ruperez@aidacanarias.com>
2022-06-10 09:18:57 +0200

    refactor

--- PasswordKata/Password.cs
+++ PasswordKata/Password.cs
@@ -1,3 +1,5 @@
+using static System.String;
+
 namespace PasswordKata;
 
 public class Password
@@ -7,24 +9,55 @@ public class Password
 
     public static string Validation(string givenPassword)
     {
-        var result = string.Empty;
+        var result = new List<string>();
 
+        var characterError = CheckCharactersInPassword(givenPassword);
+        if(!string.IsNullOrEmpty(characterError))
+            result.Add(characterError);
+        var numberError = CheckNumbersInPassword(givenPassword);
+        if(!string.IsNullOrEmpty(numberError))
+            result.Add(numberError);
+
+
+        return ComposeErrorMessage(result);
+    }
+
+    private static string ComposeErrorMessage(List<string> result)
+    {
+        var errorMessage = string.Empty;
+        for (int i = 0; i < result.Count; i++)
+        {
+            if (i == 0)
+                errorMessage += result[i];
+            else
+            {
+                errorMessage += $"\n{result[i]}";
+            }
+        }
+        return errorMessage;
+    }
+
+    private static string CheckCharactersInPassword(string givenPassword)
+    {
         if (givenPassword.Length < 8)
-            result += NOT_ENOUGH_CHARACTERS_ERROR;
+            return NOT_ENOUGH_CHARACTERS_ERROR;
+        return Empty;
+    }
 
+    private static string CheckNumbersInPassword(string givenPassword)
+    {
         var numbersInPassword = 0;
+
         foreach (char c in givenPassword)
         {
             var numberResult = 0;
-            if(int.TryParse(c.ToString(), out numberResult))
+            if (int.TryParse(c.ToString(), out numberResult))
                 numbersInPassword++;
         }
 
         if (numbersInPassword < 2)
-          result +=  string.IsNullOrEmpty(result)
-                ? NOT_ENOUGH_NUMBERS_ERROR
-                : $"\n{NOT_ENOUGH_NUMBERS_ERROR}";
+            return NOT_ENOUGH_NUMBERS_ERROR;
 
-        return result;
+        return Empty;
     }
 }
