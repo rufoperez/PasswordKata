7742bc83d59043011b8150405b88e733579789f1
Rufino Perez <ruperez@aidacanarias.com>
2022-06-10 09:02:12 +0200

    red when_password_is_less_than_two_numbers_and_less_than_8_characters_return_errors

--- PasswordKataTest/PasswordKataTest.cs
+++ PasswordKataTest/PasswordKataTest.cs
@@ -8,17 +8,25 @@ namespace PasswordKataTest
         [Test]
         public void when_password_is_7_characters_lenght_return_error()
         {
-            var givenPassword = "aaaaaaa";
-            Action act = () => Password.Validation(givenPassword);
-            act.Should().Throw<ArgumentException>("Password must be at least 8 characters");
+            var givenPassword = "aaaaa11";
+            string result = Password.Validation(givenPassword);
+            result.Should().NotBeEmpty().And.Be("Password must be at least 8 characters");
         }
 
         [Test]
         public void when_password_is_less_than_two_numbers_return_error()
         {
             var givenPassword = "aaaaaaaaaaaaaaaa1";
-            Action act = () => Password.Validation(givenPassword);
-            act.Should().Throw<ArgumentException>("Password must contain at least two numbers");
+            string result = Password.Validation(givenPassword);
+            result.Should().NotBeEmpty().And.Be("Password must contain at least two numbers");
+        }
+
+        [Test]
+        public void when_password_is_less_than_two_numbers_and_less_than_8_characters_return_errors()
+        {
+            var givenPassword = "aaaaaa1";
+            string result = Password.Validation(givenPassword);
+            result.Should().NotBeEmpty().And.Be("Password must be at least 8 characters\nPassword must contain at least two numbers");
         }
     }
 }
